c80435aead558d1d0270891fb80e297dc5666ee5
Amanda Piva <amandamariapiva@gmail.com>
2026-04-08 16:36:48 -0300

    Criando interfaces

--- Application/Features/Users/Interfaces/IUserRepository.cs
+++ Application/Features/Users/Interfaces/IUserRepository.cs
@@ -13,5 +13,6 @@ namespace api_gerenciamento_tarefas.Application.Features.Users.Interfaces
         public Task AddAsync(User user);
         public Task UpdateAsync(User user);
         public Task DeleteAsync(Guid id);
+        public Task<User?> GetByEmailAsync(string email);
     }
 }
